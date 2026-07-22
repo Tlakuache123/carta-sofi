467b02613c43dee5ee4e34832ba76245dc0e7f87
Claudio Araujo <77213711+Tlakuache123@users.noreply.github.com>
2026-07-22 03:11:00 -0600

    fix: cloudeflare adapter Astro

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,7 +1,13 @@
 // @ts-check
-import { defineConfig } from "astro/config";
+import { defineConfig, passthroughImageService } from "astro/config";
+import cloudflare from "@astrojs/cloudflare";
 
 // https://astro.build/config
 export default defineConfig({
   output: "static",
+  adapter: cloudflare(),
+
+  image: {
+    service: passthroughImageService(),
+  },
 });
